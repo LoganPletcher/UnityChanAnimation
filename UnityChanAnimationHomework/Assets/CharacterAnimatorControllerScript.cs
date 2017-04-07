d8b868fa65b4cb8b34d078bafbff1d1ae88bb138
Logan Pletcher <lpletch95@hotmail.com>
2017-04-07 17:53:55 -0500

    Initial Comit

---
+++ UnityChanAnimationHomework/Assets/CharacterAnimatorControllerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimatorControllerScript : MonoBehaviour {
+
+    public AudioSource footsource;
+    public AudioClip footstep;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void FootDown()
+    {
+        footsource.clip = footstep;
+        footsource.Play();
+    }
+}
