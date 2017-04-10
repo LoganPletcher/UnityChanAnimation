5dc1a76b697af6fdf3af2094703ad8e1cc063312
Logan Pletcher <lpletch95@hotmail.com>
2017-04-10 11:19:28 -0500

    Made an animator, added gitignore (was removed?)

--- UnityChanAnimationHomework/Assets/Script/CharacterAnimatorControllerScript.cs
+++ UnityChanAnimationHomework/Assets/Script/CharacterAnimatorControllerScript.cs
@@ -6,14 +6,20 @@ public class CharacterAnimatorControllerScript : MonoBehaviour {
     public AudioSource footsource;
     public AudioClip footstep;
 
+    private Animator _animator;
+
 	// Use this for initialization
 	void Start () {
-
+        _animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        bool w = Input.GetKeyDown("w");
+        bool r = Input.GetKeyDown("right shift");
 
+        _animator.SetBool("Walk", w);
+        _animator.SetBool("Run", r);
 	}
 
     public void FootDown()
